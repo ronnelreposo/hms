404847dcbe684d2ff732aa27eb4717e3f79aa617
Arya <ronnel.reposo@gmail.com>
2017-08-18 13:20:51 -0700

    Show Vacant Rooms Implemented

--- hms_proto/CustomerForm.Designer.cs
+++ hms_proto/CustomerForm.Designer.cs
@@ -28,69 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.create_cust_button = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.firstname_tb = new System.Windows.Forms.TextBox();
-            this.lastname_tb = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // create_cust_button
-            // 
-            this.create_cust_button.Location = new System.Drawing.Point(97, 100);
-            this.create_cust_button.Name = "create_cust_button";
-            this.create_cust_button.Size = new System.Drawing.Size(100, 23);
-            this.create_cust_button.TabIndex = 0;
-            this.create_cust_button.Text = "Create Customer";
-            this.create_cust_button.UseVisualStyleBackColor = true;
-            this.create_cust_button.Click += new System.EventHandler(this.create_cust_button_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "First Name:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Last Name:";
-            // 
-            // firstname_tb
-            // 
-            this.firstname_tb.Location = new System.Drawing.Point(97, 38);
-            this.firstname_tb.Name = "firstname_tb";
-            this.firstname_tb.Size = new System.Drawing.Size(100, 20);
-            this.firstname_tb.TabIndex = 3;
-            // 
-            // lastname_tb
-            // 
-            this.lastname_tb.Location = new System.Drawing.Point(97, 64);
-            this.lastname_tb.Name = "lastname_tb";
-            this.lastname_tb.Size = new System.Drawing.Size(100, 20);
-            this.lastname_tb.TabIndex = 4;
-            // 
-            // CustomerForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(240, 147);
-            this.Controls.Add(this.lastname_tb);
-            this.Controls.Add(this.firstname_tb);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.create_cust_button);
-            this.Name = "CustomerForm";
-            this.Text = "Customer Info";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.create_cust_button = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.firstname_tb = new System.Windows.Forms.TextBox();
+      this.lastname_tb = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // create_cust_button
+      // 
+      this.create_cust_button.Location = new System.Drawing.Point(97, 100);
+      this.create_cust_button.Name = "create_cust_button";
+      this.create_cust_button.Size = new System.Drawing.Size(100, 23);
+      this.create_cust_button.TabIndex = 0;
+      this.create_cust_button.Text = "OK";
+      this.create_cust_button.UseVisualStyleBackColor = true;
+      this.create_cust_button.Click += new System.EventHandler(this.create_cust_button_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(31, 41);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(60, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "First Name:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(31, 67);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(61, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Last Name:";
+      // 
+      // firstname_tb
+      // 
+      this.firstname_tb.Location = new System.Drawing.Point(97, 38);
+      this.firstname_tb.Name = "firstname_tb";
+      this.firstname_tb.Size = new System.Drawing.Size(100, 20);
+      this.firstname_tb.TabIndex = 3;
+      // 
+      // lastname_tb
+      // 
+      this.lastname_tb.Location = new System.Drawing.Point(97, 64);
+      this.lastname_tb.Name = "lastname_tb";
+      this.lastname_tb.Size = new System.Drawing.Size(100, 20);
+      this.lastname_tb.TabIndex = 4;
+      // 
+      // CustomerForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(240, 147);
+      this.Controls.Add(this.lastname_tb);
+      this.Controls.Add(this.firstname_tb);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.create_cust_button);
+      this.Name = "CustomerForm";
+      this.Text = "Customer Info";
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
--- hms_proto/MainForm.cs
+++ hms_proto/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hms_proto.Records;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,37 @@ namespace hms_proto
     public MainForm()
     {
       InitializeComponent();
+
+      var roomDatabase = new List<Room>
+      {
+        new Room { No = 101, Type = RoomType.Ordinary, Status = RoomStatus.Vacant },
+        new Room { No = 103, Type = RoomType.Deluxe, Status = RoomStatus.Vacant },
+        new Room { No = 105, Type = RoomType.Ordinary, Status = RoomStatus.Occupied },
+        new Room { No = 109, Type = RoomType.Ordinary, Status = RoomStatus.Vacant }
+      };
+
+      loadVacantRooms(roomDatabase, walkIn_dataGridView);
+    }
+
+    Func<DataTable, string, DataTable> dataTableColumnAdder = (dataTable, heading) => { dataTable.Columns.Add(heading); return dataTable; };
+    Func<DataTable, string[], DataTable> dataTableRowAdder = (dataTable, row) => { dataTable.Rows.Add(row); return dataTable; };
+    Func<Room, bool> vacantRoom = room => room.Status == RoomStatus.Vacant;
+    Func<Room, string[]> toRow = room => new[] { room.No.ToString(), room.Type.ToString(), room.Status.ToString() };
+
+    DataTable loadDataTableHeadings(DataTable dataTable, string[] headings) => headings.Aggregate(dataTable, dataTableColumnAdder);
+    DataTable loadDataTableRows(DataTable dataTable, string[][] rows) => rows.Aggregate(dataTable, dataTableRowAdder);
+    string[][] toVacantRooms(List<Room> rooms) => rooms.Where(vacantRoom).Select(toRow).ToArray();
+
+    DataGridView loadVacantRooms(List<Room> rooms, DataGridView dataGridView)
+    {
+      var vacantRooms = toVacantRooms(rooms);
+      var headings = new[] { "No", "Type", "Status" };
+      var dataTableWithHeadings = loadDataTableHeadings(new DataTable(), headings);
+      var dataTableWithRows = loadDataTableRows(dataTableWithHeadings, vacantRooms);
+
+      dataGridView.DataSource = dataTableWithRows.DefaultView;
+      return dataGridView;
     }
   }
+
 }
