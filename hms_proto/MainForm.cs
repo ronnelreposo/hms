7c102d1a6e6041919e9892f232dab7303025938a
Arya <ronnel.reposo@gmail.com>
2017-08-18 16:35:29 -0700

    Customer Info Included

--- hms_proto/MainForm.cs
+++ hms_proto/MainForm.cs
@@ -11,42 +11,42 @@ using System.Windows.Forms;
 
 namespace hms_proto
 {
-  public partial class MainForm: Form
-  {
-    public MainForm()
+    public partial class MainForm: Form
     {
-      InitializeComponent();
+        public MainForm()
+        {
+            InitializeComponent();
 
-      var roomDatabase = new List<Room>
-      {
-        new Room { No = 101, Type = RoomType.Ordinary, Status = RoomStatus.Vacant },
-        new Room { No = 103, Type = RoomType.Deluxe, Status = RoomStatus.Vacant },
-        new Room { No = 105, Type = RoomType.Ordinary, Status = RoomStatus.Occupied },
-        new Room { No = 109, Type = RoomType.Ordinary, Status = RoomStatus.Vacant }
-      };
+            var roomDatabase = new List<Room> {
+                new Room { No = 101, Type = RoomType.Ordinary, Status = RoomStatus.Vacant },
+                new Room { No = 103, Type = RoomType.Deluxe, Status = RoomStatus.Vacant },
+                new Room { No = 105, Type = RoomType.Ordinary, Status = RoomStatus.Occupied },
+                new Room { No = 109, Type = RoomType.Ordinary, Status = RoomStatus.Vacant }
+            };
 
-      loadVacantRooms(roomDatabase, walkIn_dataGridView);
+            loadVacantRooms(roomDatabase, walkIn_dataGridView);
+        }
+
+        Func<DataTable, string, DataTable> dataTableColumnAdder = (dataTable, heading) => { dataTable.Columns.Add(heading); return dataTable; };
+        Func<DataTable, string[], DataTable> dataTableRowAdder = (dataTable, row) => { dataTable.Rows.Add(row); return dataTable; };
+        Func<Room, bool> vacantRoom = room => room.Status == RoomStatus.Vacant;
+        Func<Room, string[]> toRow = room => new[] { room.No.ToString(), room.Type.ToString(), room.Status.ToString() };
+
+        DataTable loadDataTableHeadings(DataTable dataTable, string[] headings) => headings.Aggregate(dataTable, dataTableColumnAdder);
+        DataTable loadDataTableRows(DataTable dataTable, string[][] rows) => rows.Aggregate(dataTable, dataTableRowAdder);
+        string[][] toVacantRooms(List<Room> rooms) => rooms.Where(vacantRoom).Select(toRow).ToArray();
+
+        DataGridView loadVacantRooms(List<Room> rooms, DataGridView dataGridView)
+        {
+            var vacantRooms = toVacantRooms(rooms);
+            var headings = new[] { "No", "Type", "Status" };
+            var dataTableWithHeadings = loadDataTableHeadings(new DataTable(), headings);
+            var dataTableWithRows = loadDataTableRows(dataTableWithHeadings, vacantRooms);
+
+            dataGridView.DataSource = dataTableWithRows.DefaultView;
+            return dataGridView;
+        }
+
+
     }
-
-    Func<DataTable, string, DataTable> dataTableColumnAdder = (dataTable, heading) => { dataTable.Columns.Add(heading); return dataTable; };
-    Func<DataTable, string[], DataTable> dataTableRowAdder = (dataTable, row) => { dataTable.Rows.Add(row); return dataTable; };
-    Func<Room, bool> vacantRoom = room => room.Status == RoomStatus.Vacant;
-    Func<Room, string[]> toRow = room => new[] { room.No.ToString(), room.Type.ToString(), room.Status.ToString() };
-
-    DataTable loadDataTableHeadings(DataTable dataTable, string[] headings) => headings.Aggregate(dataTable, dataTableColumnAdder);
-    DataTable loadDataTableRows(DataTable dataTable, string[][] rows) => rows.Aggregate(dataTable, dataTableRowAdder);
-    string[][] toVacantRooms(List<Room> rooms) => rooms.Where(vacantRoom).Select(toRow).ToArray();
-
-    DataGridView loadVacantRooms(List<Room> rooms, DataGridView dataGridView)
-    {
-      var vacantRooms = toVacantRooms(rooms);
-      var headings = new[] { "No", "Type", "Status" };
-      var dataTableWithHeadings = loadDataTableHeadings(new DataTable(), headings);
-      var dataTableWithRows = loadDataTableRows(dataTableWithHeadings, vacantRooms);
-
-      dataGridView.DataSource = dataTableWithRows.DefaultView;
-      return dataGridView;
-    }
-  }
-
 }
--- hms_proto/Records/Customer.cs
+++ hms_proto/Records/Customer.cs
@@ -2,17 +2,8 @@
 {
     internal struct Customer
     {
-        internal int _id { get; private set; }
-        internal string _firstname { get; private set; }
-        internal string _lastname { get; private set; }
-        Customer(int id, string firstname, string lastname)
-        {
-            _id = id;
-            _firstname = firstname;
-            _lastname = lastname;
-        }
-        internal static Customer Create(int id, string firstname, string lastname) => new Customer(id, firstname, lastname);
-        internal Customer withFirstName(string firstname) => new Customer(_id, firstname, _lastname);
-        internal Customer withLastName(string lastname) => new Customer(_id, _firstname, lastname);
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+        internal string Phone { get; set; }
     }
 }
