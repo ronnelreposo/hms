dfefe0ee17f7d27c5b9a7a41426191248c2318b5
Arya <ronnel.reposo@gmail.com>
2017-08-18 00:54:06 -0700

    -

--- hms_proto/SignInForm.cs
+++ hms_proto/SignInForm.cs
@@ -2,13 +2,8 @@
 using hms_proto.Records;
 using hms_proto.Utils;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace hms_proto
@@ -26,7 +21,34 @@ namespace hms_proto
     private void signIn_button_Click(object sender, EventArgs e)
     {
       var errLabels = new[] { errUsername_label, errPassword_label };
-      Util.clearLabels(errLabels);
+      var clearedErrorLabels = Util.clearLabels(errLabels);
+
+      var username = username_tb.Text;
+      var password = password_tb.Text;
+
+      var rawValues = new[] { username, password };
+      Func<string, string> trim = str => str.Trim();
+      var values = rawValues.Select(trim);
+      
+      var fieldsAndValues = clearedErrorLabels.Zip(values, Tuple.Create).ToArray();
+      
+      Func<Tuple<Label, string>, bool> emptyField = fieldAndValue => string.IsNullOrEmpty(fieldAndValue.Item2);
+      Func<string, Func<Label, Label>> changeText = text => label => { label.Text = text; return label; };
+      Action<Label[]> showRequiredField = labels => { labels.Select(changeText("is required.")).ToArray(); };
+      var hasOneEmptyFieldValue = Util.IsValidValuedField(
+        Validation: emptyField,
+        OnInValidField: showRequiredField,
+        ErrorFieldsAndValues: fieldsAndValues);
+      if (hasOneEmptyFieldValue) { return; }
+
+      var MinChars = 6;
+      Func<Tuple<Label, string>, bool> shortValueField = fieldAndValue => fieldAndValue.Item2.Length < MinChars;
+      Action<Label[]> showShortField = labels => { labels.Select(changeText("is too short.")).ToArray(); };
+      var hasOneShortFieldValue = Util.IsValidValuedField(
+        Validation: shortValueField,
+        OnInValidField: showShortField,
+        ErrorFieldsAndValues: fieldsAndValues);
+      if (hasOneShortFieldValue) { return; }
 
       var account = new Account { Username = username_tb.Text, Password = password_tb.Text };
 
@@ -34,6 +56,12 @@ namespace hms_proto
       Predicate<Account> matchUsername = acc => acc.Username.Equals(acc.Username);
       var accountExist = Accounts.Exists(matchUsername);
       if (!accountExist) { MessageBox.Show("Your account is not registered."); return; }
+
+      #region Clear all Textbox contents.
+      var textBoxFields = new[] { username_tb, password_tb };
+      Func<TextBox, TextBox> clearTextBox = TextBox => { TextBox.Clear(); return TextBox; };
+      textBoxFields.Select(clearTextBox).ToArray();
+      #endregion
 
       MessageBox.Show("Welcome.");
     }
--- hms_proto/Utils/Util.cs
+++ hms_proto/Utils/Util.cs
@@ -8,5 +8,27 @@ namespace hms_proto.Utils
   {
     internal static Func<Label, Label> clearLabel = Label => { Label.Text = string.Empty; return Label; };
     internal static Label[] clearLabels(Label[] Labels) => Labels.Select(clearLabel).ToArray();
+    
+    /// <summary>
+    /// Checks if the value is valid using Validation.
+    /// if the value is invalid, then OnInValidField is invoked,
+    /// otherwise none.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Validation">The validator.</param>
+    /// <param name="OnInValidField">The action invoked if has invalid values.</param>
+    /// <param name="ErrorFieldsAndValues">A pair of values T and string. Representing Field and Value.</param>
+    /// <returns>true if has invalid value(s).</returns>
+    internal static bool IsValidValuedField<T>(Func<Tuple<T, string>, bool> Validation, Action<T[]> OnInValidField, Tuple<T, string>[] ErrorFieldsAndValues)
+    {
+      var invalidLabels = ErrorFieldsAndValues.Where(Validation);
+      var hasInvalids = invalidLabels.Count() > 0;
+      if (hasInvalids) {
+        Func<Tuple<T, string>, T> firstItem = tuple => tuple.Item1;
+        OnInValidField(invalidLabels.Select(firstItem).ToArray());
+        return true;
+      }
+      return false;
+    } /* end isValidValuedField */
   }
 }
