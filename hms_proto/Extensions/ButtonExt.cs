add7363f19b8a3d9fbc6022febb516c33e59a8b5
ronnelreposo <ronnel.reposo@gmail.com>
2017-11-24 17:32:13 -0800

    Adding ButtonExt Behavior
    Added BehaviorEnable

--- hms_proto/Extensions/ButtonExt.cs
+++ hms_proto/Extensions/ButtonExt.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Windows.Forms;
 
 namespace hms_proto.Extensions
 {
     static class ButtonExt
     {
-        internal static IObservable<EventPattern<EventArgs>> StreamClick (this Button button) => Observable.FromEventPattern(button, "Click");
+        internal static IObservable<EventPattern<EventArgs>> StreamClick (this Button button) =>
+            Observable.FromEventPattern(button, "Click");
+
+        internal static BehaviorSubject<bool> BehaviorEnable (this Button button, bool enable) =>
+            new BehaviorSubject<bool>(enable);
+
+        internal static BehaviorSubject<bool> BehaviorEnable (this Button button, bool enable, IObservable<bool> sEnable)
+        {
+            var subj = button.BehaviorEnable(enable);
+            sEnable.Subscribe(subj);
+            subj.Subscribe(b => button.Enabled = b);
+
+            return subj;
+        }
     }
 }
