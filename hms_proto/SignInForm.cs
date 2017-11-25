e7ab1ac1ba91a284d8d294a5c02b3dba89f03680
ronnelreposo <ronnel.reposo@gmail.com>
2017-11-24 17:42:51 -0800

    Code Refactoring in SignInForm
     Code Changes
    
      - Global declaration of the following:
       - Threshold
       - ShortValue
       - HasStringErr
      - Direct Enabling/Disabling SignInButton
      - Data Transformation before subscription

--- hms_proto/SignInForm.cs
+++ hms_proto/SignInForm.cs
@@ -10,15 +10,15 @@ namespace hms_proto {
 
     public partial class SignInForm: Form
     {
+        internal static readonly int Threshold = 6;
+        internal static readonly string Required = "Required";
+        internal static readonly string ShortValue = $"Must at least {Threshold} characters.";
+        internal static readonly Func<string, string> hasStrError = str => str.HasValue() ?
+                ( str.IsLengthLessThan(Threshold) ? ShortValue : string.Empty ) : Required;
+
         public SignInForm ()
         {
             InitializeComponent();
-
-            const int Threshold = 6;
-            const string Required = "Required";
-            var ShortValue = $"Must at least {Threshold} characters.";
-
-            Func<string, string> hasStrError = str => str.HasValue() ?  ( str.IsLengthLessThan(Threshold) ? ShortValue : string.Empty ) : Required;
 
             var sUsernameError = username_tb.StreamStringTextChanged().Select(hasStrError);
             sUsernameError.Subscribe(str => errUsername_label.Text = str);
@@ -27,27 +27,37 @@ namespace hms_proto {
             sPasswordError.Subscribe(str => errPassword_label.Text = str);
 
             Func<string, bool> hasError = err => err.Equals(string.Empty);
-            var sHasNoError = sUsernameError
+            sUsernameError
                 .Select(hasError)
-                .CombineLatest(sPasswordError.Select(hasError), (a, b) => a && b);
-            sHasNoError.Subscribe(hasNoError => signIn_button.Enabled = hasNoError);
+                .CombineLatest(sPasswordError.Select(hasError), (a, b) => a && b)
+                .Subscribe(b => signIn_button.Enabled = b);
 
             var sEventThisLoad = this.StreamEventLoad();
-            sEventThisLoad.Subscribe(_ => Util.clearLabels(errUsername_label, errPassword_label));
-            sEventThisLoad.Subscribe(_ => signIn_button.Enabled = false);
+            sEventThisLoad
+                .Select(_ => new[] { errUsername_label, errPassword_label })
+                .Subscribe(labels => Util.clearLabels(labels));
+            sEventThisLoad
+                .Select(_ => false)
+                .Subscribe(no => signIn_button.Enabled = no);
 
             var sExist = signIn_button
                 .StreamClick()
                 .Select(_ => Account.Create(Username: username_tb.Text, Password: password_tb.Text))
                 .Select(account => AccountDatabase.Accounts.Exists(account_ => account_.Equals(account)));
 
-            var sAccountNonExist = sExist.Where(accountExist => accountExist.Equals(false));
-            sAccountNonExist.Subscribe(_ => password_tb.Clear());
-            sAccountNonExist.Subscribe(_ => MessageBox.Show("Your account is not registered."));
+            var sAccountNonExist = sExist
+                .Where(accountExist => accountExist.Equals(false));
+            sAccountNonExist
+                .Select(_ => password_tb)
+                .Subscribe(textBox => textBox.Clear());
+            sAccountNonExist
+                .Select(_ => "Your account is not registered.")
+                .Subscribe(message => MessageBox.Show(message));
 
-            var sAccountExist = sExist.Where(accountExist => accountExist.Equals(true));
-            sAccountExist.Subscribe(_ => new MainForm().UShow());
-            sAccountExist.Subscribe(_ => this.UHide());
+            var sAccountExist = sExist
+                .Where(accountExist => accountExist.Equals(true));
+            sAccountExist.Select(_ => new MainForm()).Subscribe(form => form.UShow());
+            sAccountExist.Select(_ => this).Subscribe(form => form.UHide());
 
             register_button
                 .StreamClick()
